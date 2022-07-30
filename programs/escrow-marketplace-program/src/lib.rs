0c83a87e5a7e45af6057bd49017e5ce165719e27
jakeyvee <jakeyveee@gmail.com>
2022-07-30 19:40:51 +0800

    define create_listing in lib.rs

--- programs/escrow-marketplace-program/src/lib.rs
+++ programs/escrow-marketplace-program/src/lib.rs
@@ -8,9 +8,11 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod escrow_marketplace_program {
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_listing(ctx: Context<CreateListing>, list_price: u128, escrow_info_bump: u8) -> Result<()> {
+        instructions::create_listing::handler(ctx, list_price, escrow_info_bump)?;
         Ok(())
     }
 }
