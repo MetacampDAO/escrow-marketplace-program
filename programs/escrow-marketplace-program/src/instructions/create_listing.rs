3424f9cccbfa6d05f8093e9bdbc4f9af92c9b402
jakeyvee <jakeyveee@gmail.com>
2022-07-30 19:17:32 +0800

    define CreateListing methods

--- programs/escrow-marketplace-program/src/instructions/create_listing.rs
+++ programs/escrow-marketplace-program/src/instructions/create_listing.rs
@@ -1,7 +1,8 @@
 use anchor_lang::prelude::*;
 
 use crate::states::*;
-use anchor_spl::token::{Mint, TokenAccount};
+use spl_token::instruction::AuthorityType;
+use anchor_spl::token::{self, Mint, SetAuthority, TokenAccount, Transfer};
 
 #[derive(Accounts)]
 pub struct CreateListing<'info>{
@@ -26,9 +27,37 @@ pub struct CreateListing<'info>{
       token::mint = nft_mint,
       token::authority = seller,
   )]
-  pub vault_account: Account<'info, TokenAccount>,
+  pub escrow_token: Account<'info, TokenAccount>,
   pub system_program: Program<'info, System>,
   pub rent: Sysvar<'info, Rent>,
   /// CHECK: This is not dangerous because we don't read or write from this account
   pub token_program: AccountInfo<'info>,
+}
+
+impl <'info> CreateListing <'info> {
+  fn transfer_to_escrow_token(&self) -> Result<()> {
+      let cpi_accounts = Transfer {
+          from: self
+              .seller_token
+              .to_account_info()
+              .clone(),
+          to: self.escrow_token.to_account_info().clone(),
+          authority: self.seller.to_account_info().clone(),
+      };
+      token::transfer(CpiContext::new(self.token_program.clone(), cpi_accounts), 1);
+      Ok(())
+  }
+
+  fn set_authority_to_escrow_info(&self) -> Result<()> {
+      let cpi_accounts = SetAuthority {
+          account_or_mint: self.escrow_token.to_account_info().clone(),
+          current_authority: self.seller.to_account_info().clone(),
+      };
+      token::set_authority(
+        CpiContext::new(self.token_program.clone(), cpi_accounts), 
+        AuthorityType::AccountOwner, 
+        Some(self.escrow_info.key())
+      );
+      Ok(())
+  }
 }
